6b07d29d0f5437d2c5f1a0c50aaa02c1e87ac1f6
JorgeHe22 <jorgex.herrera24@gmail.com>
2025-02-14 14:05:10 -0500

    Operaciones basicas

--- src/Main.kt
+++ src/Main.kt
@@ -4,6 +4,12 @@ fun main() {
     println("Jorge Herrera")
     println("Jugar xbox")
 
+    println("Esta es mi rama de desarrollo")
+    println("suma")
+    println("resta")
+    println("multiplicacion")
+    println("division")
+
 
 
 }
